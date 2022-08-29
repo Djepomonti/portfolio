9b91644faa0edd5fdcdeefd4843f5d1baafbdf4f
jerome pomonti <jerome.pomonti@gmail.com>
2022-08-29 20:30:30 +0200

    prod

---
+++ precache-manifest.53ecee7a12c134bd13b996c4a50e0016.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "302449ef9d5fab156af0b2cd061ac343",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4177f1ce612190a8628d",
+    "url": "/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "e585529cdd9b82fc1fab",
+    "url": "/static/css/main.db6f0745.chunk.css"
+  },
+  {
+    "revision": "4177f1ce612190a8628d",
+    "url": "/static/js/2.92e64c3c.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/static/js/2.92e64c3c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e585529cdd9b82fc1fab",
+    "url": "/static/js/main.8601c833.chunk.js"
+  },
+  {
+    "revision": "a2d41c56873c741b51b2",
+    "url": "/static/js/runtime-main.a5042f6b.js"
+  }
+]);
